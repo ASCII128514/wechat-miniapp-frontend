dbdc707402bcda40ec8c8c99d272eb24286c2f6c
Paul Lu <luhanfeng1106@sina.com>
2019-04-26 14:44:05 +0800

    default pic

--- utils/api_client.js
+++ utils/api_client.js
@@ -41,7 +41,7 @@ const createProduct = function (page, p) {
           "unit": page.detail.value.unit,
           "product_price": page.detail.value.price,
           "product_name": page.detail.value.name,
-          "picture_url": p.data.url,
+          "picture_url": p.data.url || "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4266087431,2209117257&fm=200&gp=0.jpg",
           "description": page.detail.value.description
         },
         "quantity": page.detail.value.inventory
